97d281d5ee4b9930675767924d2422001e34e51b
AthibanNatarajan <74203408+AthibanNatarajan@users.noreply.github.com>
2021-12-09 01:23:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+
+Vid = cv2.VideoCapture(0)
+face_cas = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+jas=cv2.imread("Jason_mask.png")
+while True:
+    ret, frame = Vid.read()
+    face=face_cas.detectMultiScale(frame,scaleFactor=1.2,minNeighbors=4)
+    for (x, y, w, h) in face:
+        jas_siz=cv2.resize(jas,(w,h))
+        cv2.imshow('Jason Mask',jas_siz)
+        _,face_mask=cv2.threshold(cv2.cvtColor(jas_siz,cv2.COLOR_BGR2GRAY),25,255,cv2.THRESH_BINARY_INV)
+        face_rect=frame[y:y+h,x:x+w]
+        op=cv2.bitwise_and(face_rect,face_rect,mask=face_mask)
+        cv2.imshow('Mask',face_mask)
+        cv2.imshow('face alone',face_rect)
+        cv2.imshow('output',op)
+        plus=cv2.add(op,jas_siz)
+        frame[y:y + h, x:x + w]=plus
+    cv2.imshow('Face Detection', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
